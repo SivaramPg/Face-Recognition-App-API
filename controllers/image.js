f506e4244877ce06d5d85d44790c3d55502b40b6
SivaramPg <sivaramp95@gmail.com>
2018-07-05 21:08:32 +0530

    fixing FaceBox

--- controllers/image.js
+++ controllers/image.js
@@ -6,7 +6,7 @@ const app = new Clarifai.App({
 
 const handleApiCall = (req, res) => {
 app.models
-  .predict(Clarifai.FACE_DETECT_MODEL, req,body.input)
+  .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
   .then(res => {
     res.json(data);
   })
