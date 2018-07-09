3e493ea4d303b1bde7881756eb85f2d06632633e
SivaramPg <sivaramp95@gmail.com>
2018-07-09 19:45:45 +0530

    changed clarifai api id for demographic prediction

--- controllers/image.js
+++ controllers/image.js
@@ -6,7 +6,7 @@ const app = new Clarifai.App({
 
 const handleApiCall = (req, res) => {
 app.models
-  .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+  .predict("0c0ac362b03416da06ab3fa36fb58e3", req.body.input)
   .then(data => {
     res.json(data);
   })
