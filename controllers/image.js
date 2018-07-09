263274b910bf9a80cb6f75df1307eaf880bac7c5
SivaramPg <sivaramp95@gmail.com>
2018-07-09 19:51:31 +0530

    fixed model id in predict

--- controllers/image.js
+++ controllers/image.js
@@ -6,7 +6,7 @@ const app = new Clarifai.App({
 
 const handleApiCall = (req, res) => {
 app.models
-  .predict("0c0ac362b03416da06ab3fa36fb58e3", req.body.input)
+  .predict("c0c0ac362b03416da06ab3fa36fb58e3", req.body.input)
   .then(data => {
     res.json(data);
   })
